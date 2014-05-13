63a7449f2e661290c078233a2cf015a342b41246
Joe Hamman <jhamman@hydro.washington.edu>
2014-05-13 11:32:02 -0400

    start using netCDF4-python for some metadata operations and changes to SPIRIT mach_opts.

--- post_processing/share.py
+++ post_processing/share.py
@@ -24,8 +24,7 @@ NCOFORMAT = "%Y-%m-%d %H:%M:%S"
 __GARNET_OPTS__ = {}
 
 __SPIRIT_OPTS__ = {'no_tmp_fl': True,
-                   'ram_all': True,
-                   'omp_num_threads': 4}
+                   'ram_all': True}
 
 __HYDRA_OPTS__ = {'no_tmp_fl': True,
                   'ram_all': True,
