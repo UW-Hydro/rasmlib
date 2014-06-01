f8be95761ded28c499b2036f35016f32d4f1e717
Joe Hamman <jhamman@hydro.washington.edu>
2014-06-01 16:51:02 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+ #!/usr/bin/env python
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+# -------------------------------------------------------------------- #
+# Run Setup
+setup(name='rasmlib',
+      version='0.0.0',
+      description='Tools library for the Regional Arctic System Model',
+      long_description='',
+      author='Joe Hamman',
+      author_email='jhamman@hydro.washington.edu',
+      install_requires=['scipy >= 0.13', 'numpy >= 1.8',
+                        'netCDF4 >= 1.0.6', 'matplotlib >= 1.3.1'],
+      url='https://github.com/jhamman/rasmlib',
+      packages=['rasmlib.analysis', 'rasmlib.post_processing',
+                'rasmlib.tools'],
+      py_modules=['rasmlib.analysis.plugins'],
+      scripts=['scripts/rasm_analysis', 'scripts/rasm_post_process'])
+# -------------------------------------------------------------------- #
